872231cbe19bb5f262d324e077c68f7048da7250
Sarthak Roy <p-2066-19@dpspatna.com>
2025-12-07 17:26:51 +0530

    Add bidirectional model and code, notes

---
+++ biderctional_lstm.py
@@ -0,0 +1,87 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import DataLoader
+import torchvision.datasets as datasets
+import torchvision.transforms as transforms
+
+device = torch.device("cpu")
+
+batch = 64
+num_classes = 10
+input_size = 28
+sequence_length = 28
+num_layers = 2
+hidden_size = 256
+learning_rate = 0.001
+epochs = 4
+
+class B_LSTM(nn.Module):
+    def __init__(self, input_size, hidden_size, num_layers, num_classes):
+        super(B_LSTM, self).__init__()
+        self.hidden_size = hidden_size
+        self.num_layers = num_layers
+        self.lstm = nn.LSTM(input_size, hidden_size, num_layers, batch_first=True, bidirectional=True)
+        self.fc = nn.Linear(hidden_size*2, num_classes)
+    
+    def forward(self, x):
+        h0 = torch.zeros(self.num_layers*2, x.size(0), self.hidden_size).to(device)
+        c0 = torch.zeros(self.num_layers*2, x.size(0), self.hidden_size).to(device)
+        out, _ = self.lstm(x, (h0, c0))
+        out = out[:, -1, :]
+        out = self.fc(out)
+        return out
+
+train_dataset = datasets.MNIST(root="dataset/", train=True, transform=transforms.ToTensor(), download=False)
+train_loader = DataLoader(dataset=train_dataset, batch_size=batch, shuffle=True)
+
+test_dataset = datasets.MNIST(root="dataset/", train=False, transform=transforms.ToTensor(), download=False)
+test_loader = DataLoader(dataset=test_dataset, batch_size=batch, shuffle=True)
+
+model = B_LSTM(input_size, hidden_size, num_layers, num_classes).to(device)
+
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.Adam(model.parameters(), lr=learning_rate)
+
+for epoch in range(epochs):
+    for batch_idx, (data, targets) in enumerate(train_loader):
+        data = data.to(device).squeeze(1)
+        targets = targets.to(device)
+        scores = model(data)
+        loss = criterion(scores, targets)
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+def check_accuracy(loader, model):
+    if loader.dataset.train:
+        print("Checking accuracy on the training data..")
+    else:
+        print("Checking accuracy on the testing data..")
+    num_correct = 0
+    num_samples = 0
+    model.eval()
+    with torch.no_grad():
+        for x, y in loader:
+            x = x.to(device).squeeze(1)
+            y = y.to(device)
+            scores = model(x)
+            _, predictions = scores.max(1)
+            num_correct += (predictions == y).sum()
+            num_samples += predictions.size(0)
+        print(f"Got {num_correct}/{num_samples} with accuracy {float(num_correct)/float(num_samples)*100:.2f}")
+    model.train()
+
+check_accuracy(test_loader, model)
+
+def save_model(model):
+    """
+    Saves the trained model to a file.
+ 
+    
+    :param model: Description
+    """
+    torch.save(model.state_dict(), "b_lstm_model.pth")
+    print("Model saved to lstm_model.pth")
+
+save_model(model)
